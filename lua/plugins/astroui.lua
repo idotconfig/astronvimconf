6e2b8e84b52d7b3e88b51f73b9a875f605369f96
~/.config <aka.config@proton.me>
2026-04-03 08:41:17 +0600

    add custom color theme

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -4,7 +4,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "carbon-blaze",
+    colorscheme = "ayu-dark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
